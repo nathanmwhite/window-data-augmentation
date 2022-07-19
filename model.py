af81c952f845872199b804e3e35c0ffe080f6bd3
nathanmwhite <53862826+nathanmwhite@users.noreply.github.com>
2022-07-20 09:17:45 +1000

    Create model.py

---
+++ model.py
@@ -0,0 +1,20 @@
+# model.py
+# Original code Copyright © 2022 Nathan M. White
+
+# Copyright notice
+__copyright__ = "Copyright © 2022 Nathan M. White"
+
+# author
+__author__ = "Nathan M. White"
+__author_email__ = "nathan.white1@jcu.edu.au"
+
+from torch.nn import Transformer
+
+# TODO: implement the following:
+# 1. Transformer (with four variants)
+# 2. BiLSTM
+# 3. GRU
+# 4. CNN
+# 5. If time: Multilingual BERT
+# Note: if using this approach, other files need to be redone
+#  to support PyTorch
