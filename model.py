163e748dbcde37f2bdc9254ac60caf4bf0d8d533
nathanmwhite <53862826+nathanmwhite@users.noreply.github.com>
2022-07-21 14:10:40 +1000

    Update model.py

--- model.py
+++ model.py
@@ -18,6 +18,15 @@ from torch.nn import Embedding, Linear, LSTM, MultiheadAttention, Transformer
 # 4. CNN
 # 5. If time: Multilingual BERT
 
+# TODO: finish implementation
+class BahdanauAttention(Module):
+    def __init__(self, s_dim, h_dim):
+        super(BahdanauAttention, self).__init__()
+        self.s_dim = s_dim
+        self.h_dim = h_dim
+        
+        
+
 class BilstmEncoder(Module):
     def __init__(self, input_dim, hidden_dim, output_dim, num_attention_heads):
         super(BilstmEncoder, self).__init__()
@@ -33,6 +42,7 @@ class BilstmEncoder(Module):
                                  bidirectional=True,
                                  batch_first=True)
         # BiLSTM output is (batch size, sequence length, 2*hidden_dim
+        # TODO: review to determine whether attention layer should be moved
         self.attention = MultiheadAttention(self.hidden_dim, num_attention_heads)
         # TODO: how should encoder hidden state be handled?
 #        self.dense_layer = Linear(in_features=self.
@@ -43,4 +53,5 @@ class BilstmDecoder(Module):
         super(BilstmDecoder, self).__init__()
         self.hidden_dim = hidden_dim
         
+        # TODO: implement hidden state, s, and how to output to y
         
