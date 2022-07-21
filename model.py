8547855b7d5d5e43558c424ccc823b96c6654ac0
nathanmwhite <53862826+nathanmwhite@users.noreply.github.com>
2022-07-21 13:28:52 +1000

    Update model.py

--- model.py
+++ model.py
@@ -8,7 +8,8 @@ __copyright__ = "Copyright © 2022 Nathan M. White"
 __author__ = "Nathan M. White"
 __author_email__ = "nathan.white1@jcu.edu.au"
 
-from torch.nn import Embedding, Linear, LSTM, Module, Transformer
+from torch.nn import Module
+from torch.nn import Embedding, Linear, LSTM, MultiheadAttention, Transformer
 
 # TODO: implement the following:
 # 1. Transformer (with four variants)
@@ -18,7 +19,7 @@ from torch.nn import Embedding, Linear, LSTM, Module, Transformer
 # 5. If time: Multilingual BERT
 
 class BilstmEncoder(Module):
-    def __init__(self, input_dim, hidden_dim, output_dim):
+    def __init__(self, input_dim, hidden_dim, output_dim, num_attention_heads):
         super(BilstmEncoder, self).__init__()
         self.hidden_dim = hidden_dim
         
@@ -32,6 +33,14 @@ class BilstmEncoder(Module):
                                  bidirectional=True,
                                  batch_first=True)
         # BiLSTM output is (batch size, sequence length, 2*hidden_dim
+        self.attention = MultiheadAttention(self.hidden_dim, num_attention_heads)
         # TODO: how should encoder hidden state be handled?
 #        self.dense_layer = Linear(in_features=self.
 #                                  out_features=output_dim)
+
+class BilstmDecoder(Module):
+    def __init__(self, input_dim, hidden_dim, output_dim):
+        super(BilstmDecoder, self).__init__()
+        self.hidden_dim = hidden_dim
+        
+        
