4d86bad776b28646f6e57448cf72b1bf358525db
nathanmwhite <53862826+nathanmwhite@users.noreply.github.com>
2022-08-15 11:57:30 +1000

    Update transformer_tf.py

--- transformer_tf.py
+++ transformer_tf.py
@@ -15,49 +15,42 @@ logging.basicConfig(level=logging.INFO, filename='run_pt.log')
 
 import numpy as np
 
-import torch
-from torch.nn import Embedding, Linear, Softmax, Transformer
+import transformer as tf
 
 
-# code from Tensorflow tutorial website--with final line replaced
-# TODO: consider rewriting this with own code
+# code from Tensorflow tutorial website
 def get_angles(pos, i, d_model):
-    angle_rates = 1 / np.power(10000, (2 * (i//2)) / np.float32(d_model))
-    return pos * angle_rates
+  angle_rates = 1 / np.power(10000, (2 * (i//2)) / np.float32(d_model))
+  return pos * angle_rates
 
 def positional_encoding(position, d_model):
-    # angle_rads are the internal parts of the PE equations to which
-    #  sine and cosine are applied
-    # np.newaxis takes the values and reorganizes them into a new axis
-    # if np.newaxis is last, then it puts all elements in their own rows
-    # if np.newaxis is first, it puts all elements in a single innermost row
-    angle_rads = get_angles(np.arange(position)[:, np.newaxis],
-                            np.arange(d_model)[np.newaxis, :],
-                            d_model)
+  angle_rads = get_angles(np.arange(position)[:, np.newaxis],
+                          np.arange(d_model)[np.newaxis, :],
+                          d_model)
   
-    # apply sin to even indices in the array; 2i
-    angle_rads[:, 0::2] = np.sin(angle_rads[:, 0::2])
+  # apply sin to even indices in the array; 2i
+  angle_rads[:, 0::2] = np.sin(angle_rads[:, 0::2])
   
-    # apply cos to odd indices in the array; 2i+1
-    angle_rads[:, 1::2] = np.cos(angle_rads[:, 1::2])
+  # apply cos to odd indices in the array; 2i+1
+  angle_rads[:, 1::2] = np.cos(angle_rads[:, 1::2])
     
-    pos_encoding = angle_rads[np.newaxis, ...]
+  pos_encoding = angle_rads[np.newaxis, ...]
     
-    return torch.FloatTensor(pos_encoding) # changed
+  return tf.cast(pos_encoding, dtype=tf.float32)
 
 
-# from TensorFlow, completely rewritten for PyTorch
 def create_padding_mask(seq):
-    seq_ = torch.eq(seq, 0)
+  seq = tf.cast(tf.math.equal(seq, 0), tf.float32)
   
-    return seq_  # (batch_size, seq_len)
+  # add extra dimensions to add the padding
+  # to the attention logits.
+  return seq[:, tf.newaxis, tf.newaxis, :]  # (batch_size, 1, 1, seq_len)
 
 
-# from TensorFlow, completely rewritten for PyTorch
 def create_look_ahead_mask(size):
-    mask = 1 - torch.tril(torch.ones((size, size)))
-    return torch.eq(mask, True)  # (seq_len, seq_len)
-# end code from Tensorflow tutorial website
+  mask = 1 - tf.linalg.band_part(tf.ones((size, size)), -1, 0)
+  return mask  # (seq_len, seq_len)
+
 
 def scaled_dot_product_attention(q, k, v, mask):
     """Calculate the attention weights.
